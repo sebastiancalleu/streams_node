06d9255eaa0c4656135929770a5308b79f22fd66
sebastian <sebastian.calle@globant.com>
2024-09-02 15:51:14 -0500

    adding pipeline

--- transformStream.js
+++ transformStream.js
@@ -1,21 +1,50 @@
-const { Transform } = require('node:stream');
+const { Transform, Readable, Writable } = require('node:stream');
+const { pipeline } = require('node:stream/promises');
 const fs = require('node:fs');
 
 const file = fs.createWriteStream('./transformStdIn.txt');
 
-const transformStream = new Transform({
-    transform(chunk, encoding, callback) {
-        file.write(chunk.toString().replaceAll(' ', '').toUpperCase());
-        callback();
-    }
-})
+const readableStream = new Readable({
+    read() {},
+});
 
-transformStream.pipe(transformStream);
+const writableStream = new Writable({
+    write(chunk, encoding, callback) {
+        if (chunk.toString() === 'exit\n') {
+            file.end();
+            this.end();
+        } else {
+            file.write(chunk.toString().toUpperCase());
+        }
+        callback();
+    },
+});
+
+writableStream.on('pipe', () => {
+    console.log('piping to the writable stream')
+});
+
+readableStream.on('end', () => {
+    console.log('input captured successfuly')
+});
 
 process.stdin.on('data', function(chunk) {
     if (chunk.toString() == 'exit\n') {
-        this.destroy();
+        process.stdin.destroy();
+        readableStream.push(null);
     } else {
-        transformStream.push(chunk)
+        readableStream.push(chunk.toString());
     }
-})
+});
+
+const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().replaceAll(' ', '').toUpperCase());
+    }
+});
+
+pipeline(
+    readableStream,
+    transformStream,
+    writableStream
+);
